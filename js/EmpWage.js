33ec58d2981951b4c52c8ad1d8f1e3ff5ce6dd43
mayurpatil44 <mayurpatil44@hotmail.com>
2021-05-28 11:04:33 +0530

    Added Validation to Date and Name on Employee Payroll Form

--- js/EmpWage.js
+++ js/EmpWage.js
@@ -5,3 +5,24 @@ salary.addEventListener('input',function()
 {
    output.textContent=salary.value;
 });
+const text=document.querySelector('#text');
+const textError=document.querySelector('.text-error');
+text.addEventListener('input',function()
+        {
+            let nameRegex=RegExp('^[A-Z]{1}[a-z]{2,}$');
+            if(nameRegex.test(text.value))
+            textError.textContent="";
+            else 
+            textError.textContent="Name is incorrect";
+        });
+const date=document.querySelector("#date");
+const dateError = document.querySelector(".date-error");
+dateError.textContent="";
+date.addEventListener('input',function(){
+    let now = new Date();
+    if (startDate > now) 
+    dateError.textContent= "Start Date is a Future Date!";
+    var diff = Math.abs(now.getTime() - startDate.getTime());
+    if (diff / (1000 * 60 * 60 * 24) > 30) 
+     dateError.textContent= "Start Date is beyond 30 Days!";
+});
